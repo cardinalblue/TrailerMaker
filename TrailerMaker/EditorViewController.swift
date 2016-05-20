54a0b810f504994b9cd1d38c93eb231f0b0fa1d4
wani <wani420@gmail.com>
2016-05-20 12:50:58 +0800

    Finish the basic layout

--- TrailerMaker/EditorViewController.swift
+++ TrailerMaker/EditorViewController.swift
@@ -11,18 +11,41 @@ import SnapKit
 
 class EditorViewController: UIViewController {
 
-    lazy var box = UIView()
+    lazy var recordView = UIView() // top left
+    lazy var trailerView = UIView() // top right
+    lazy var timelineScrollView = UIScrollView()
     lazy var bottomToolBar = UIToolbar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+      
         self.view.addSubview(bottomToolBar)
-        
         bottomToolBar.snp_makeConstraints { make in
             make.leading.trailing.bottom.equalTo(self.view)
             make.height.equalTo(44)
         }
+        
+        self.view.addSubview(timelineScrollView)
+        timelineScrollView.snp_makeConstraints { make in
+            make.leading.trailing.equalTo(self.view)
+            make.bottom.equalTo(bottomToolBar.snp_top)
+            make.height.equalTo(44)
+        }
+        
+        self.view.addSubview(recordView)
+        recordView.snp_makeConstraints { make in
+            make.leading.top.equalTo(self.view)
+            make.bottom.equalTo(timelineScrollView.snp_top)
+            make.width.equalTo(self.view).dividedBy(2)
+        }
+        
+        self.view.addSubview(trailerView)
+        trailerView.snp_makeConstraints { make in
+            make.trailing.top.equalTo(self.view)
+            make.bottom.equalTo(timelineScrollView.snp_top)
+            make.width.equalTo(self.view).dividedBy(2)
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
