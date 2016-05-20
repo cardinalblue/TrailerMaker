35cf82c1298c9a71d22e59c5d44dc91ea9ebceb5
Cathy Oun <cathy810218@gmail.com>
2016-05-20 12:14:59 +0800

    Add templates

---
+++ TrailerMaker/TemplateCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TemplateCollectionViewCell.swift
+//  TrailerMaker
+//
+//  Created by Cathy Oun on 5/20/16.
+//  Copyright © 2016 CBLUE. All rights reserved.
+//
+
+import UIKit
+
+class TemplateCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var templateImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- TrailerMaker/TemplateViewController.swift
+++ TrailerMaker/TemplateViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class TemplateViewController: UIViewController {
-
+class TemplateViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    var collectionView : UICollectionView!
+    var trailerCoverPhotos : [UIImage]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        let collectionCellWidth = (screenWidth - 40) / 2
+        let collectionCellHeight = (collectionCellWidth / 1.4)
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 30, left: 15, bottom: 10, right: 15)
+        layout.itemSize = CGSize(width: collectionCellWidth, height: collectionCellHeight)
+
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        let nibName = UINib(nibName: "TemplateCollectionViewCell", bundle:NSBundle.mainBundle())
+        collectionView.registerNib(nibName, forCellWithReuseIdentifier: "TemplateCell")
+        collectionView.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView)
+        
+        trailerCoverPhotos = [
+            UIImage(named: "dark_knight")!,
+            UIImage(named: "harry_potter")!,
+            UIImage(named: "hunger_game")!,
+            UIImage(named: "inception")!,
+            UIImage(named: "twilight")!
+        ]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +48,18 @@ class TemplateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: UICollectionViewDelegate
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return trailerCoverPhotos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TemplateCell", forIndexPath: indexPath) as! TemplateCollectionViewCell
+        cell.backgroundColor = UIColor.redColor()
+        cell.templateImageView.image = trailerCoverPhotos[indexPath.item]
+        cell.templateImageView.contentMode = UIViewContentMode.ScaleAspectFit
 
+        return cell
+    }
 }
 
