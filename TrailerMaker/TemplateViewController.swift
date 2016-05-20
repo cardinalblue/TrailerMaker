377e15f6b06dadc3059ccaccd8e24adce4ebc1fb
wani <wani420@gmail.com>
2016-05-20 11:08:16 +0800

    Init

---
+++ TrailerMaker/TemplateViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TemplateViewController.swift
+//  TrailerMaker
+//
+//  Created by CBLUE on 5/20/16.
+//  Copyright © 2016 CBLUE. All rights reserved.
+//
+
+import UIKit
+
+class TemplateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
